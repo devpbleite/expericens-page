3f02677304778d24cc1d5ef666bf15f595dbbdcb
Pablo Leite <pabloleite03@gmail.com>
2023-06-15 11:04:18 -0300

    Add files via upload

---
+++ src/components/Offers.jsx
@@ -0,0 +1,13 @@
+
+const Offers = () => {
+  return (
+    <div className="max-w-[900px] m-auto px-4 py-12 flex flex-wrap justify-between">
+      <p className="text-lg font-bold text-gray-700">Booking Incentives</p>
+      <p className="text-lg ">Competitive Rates</p>
+      <p className="text-lg ">Book with Flexibility</p>
+      <p className="text-lg ">Trip Insurance</p>
+    </div>
+  )
+}
+
+export default Offers
